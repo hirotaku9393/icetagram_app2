405689875f29207e266a314e192cf20e09c067f2
Your Name <you@example.com>
2025-08-02 16:34:45 +0900

    first

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        "./app/views/**/*.html.erb",
+        "./app/javascript/**/*.js",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                poller: ['"Poller One"', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
