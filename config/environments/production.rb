a029021e6428caccfde3a9cfe5841f6bd98ef6d5
Your Name <you@example.com>
2025-09-10 22:57:56 +0900

    config.action_controller.allow_forgery_protection = falseを追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   #   port: 587,
   #   authentication: :plain
   # }
-
+  config.action_controller.allow_forgery_protection = false
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
