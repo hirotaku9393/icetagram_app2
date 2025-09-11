2bd0577335b0edad037d486052c7f2d6c7e5c761
Your Name <you@example.com>
2025-09-11 15:41:20 +0900

    application.contoller.rb等修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protect_from_forgery with: :exception
   # Configure additional parameters for Devise
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
