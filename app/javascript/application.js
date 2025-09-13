642074bd6a885e94bfee23bac5b4723622c41fdc
Your Name <you@example.com>
2025-09-13 20:41:21 +0900

    application.jsの再修正修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,6 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import Chart from 'chart.js/auto';  
+window.Chart = Chart; 
 import * as ActiveStorage from "@rails/activestorage"
 ActiveStorage.start()
