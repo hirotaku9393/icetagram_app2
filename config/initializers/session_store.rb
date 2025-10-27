608b36f6e29b66cbd092801e1bb595b3965965ed
Your Name <you@example.com>
2025-10-27 10:46:34 +0900

    session_store3行目修正

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 Rails.application.config.session_store :cookie_store,
     key: '_icetagram_session',
-    domain: 'icetagram.com',
+    domain: :all,
     same_site: :lax,          
     secure: Rails.env.production?  
