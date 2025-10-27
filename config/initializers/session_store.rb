c81426d051f3d63494b1a0c1655aee75336c0003
Your Name <you@example.com>
2025-10-27 14:40:28 +0900

    session.rb にhttonly追加

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,5 @@ Rails.application.config.session_store :cookie_store,
     key: '_icetagram_session',
     domain: :all,
     same_site: :none,
-    secure: Rails.env.production?  
+    secure: Rails.env.production? ,
+    httponly: true 
