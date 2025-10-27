e864af3a32203d9e57d95b45d060d316d2033da1
Your Name <you@example.com>
2025-10-27 11:49:40 +0900

    session.rb same_site:を変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 Rails.application.config.session_store :cookie_store,
     key: '_icetagram_session',
     domain: :all,
-    same_site: :lax,          
+    same_site: :none,
     secure: Rails.env.production?  
