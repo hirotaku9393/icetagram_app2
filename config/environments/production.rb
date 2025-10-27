121b301d561910221ada4c1ad3885d3a83dffa0d
Your Name <you@example.com>
2025-10-27 14:19:13 +0900

    renderドメインをproducition.rbに追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
   config.force_ssl = true
   config.hosts << "icetagram.com"
   config.hosts << "www.icetagram.com"
+  config.hosts << "icetagram-app2.onrender.com"
 
   config.active_storage.service = :cloudinary
 
