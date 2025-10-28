8ccc31d8abfa24f2a85c9b519cf7a2438e7c2af6
Your Name <you@example.com>
2025-10-28 10:48:33 +0900

    protect_from_forgery with: :null_sessionを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,16 @@
 class ApplicationController < ActionController::Base
-  helper ApplicationHelper
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-  before_action :configure_permitted_parameters, if: :devise_controller?
+    helper ApplicationHelper
+    # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+    allow_browser versions: :modern
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protect_from_forgery with: :exception
-  # Configure additional parameters for Devise
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
+    protect_from_forgery with: :exception
+    protect_from_forgery with: :null_session
+    # Configure additional parameters for Devise
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 
     def set_ogp_tags(title:, description:, image_url:)
         set_meta_tags(
