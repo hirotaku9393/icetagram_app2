f9307b5f6779b058323dee5fe58f8e67d3844cbc
kazuta65b <hirokazu.vn.ank@gmail.com>
2025-11-04 00:46:22 +0900

    show.html修正

--- app/models/ice_cream.rb
+++ app/models/ice_cream.rb
@@ -6,6 +6,8 @@ class IceCream < ApplicationRecord
     has_many :favorites, dependent: :destroy
     has_many :ice_cream_relationships, dependent: :destroy
     has_many :tags, through: :ice_cream_relationships
+    has_many :today_ices, dependent: :destroy
+    validates :name, presence: true
     def to_vector
         [ sweetness, freshness, richness, texture, ingredient_richness ].map(&:to_i)
     end
