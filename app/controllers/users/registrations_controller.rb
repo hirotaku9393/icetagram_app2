ba06161a43374bc0d904f74d4bd72aee26ed44ae
kazuta65b <hirokazu.vn.ank@gmail.com>
2025-11-09 19:17:08 +0900

    registorコントローラ修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+    skip_before_action :verify_authenticity_token, only: [:create], if: :oauth_request?
+
     def build_resource(hash = {})
         hash[:uid] = User.create_unique_string
         super
@@ -6,11 +8,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     def update_resource(resource, params)
         if resource.provider.present? # グーグル、ラインログインはパスワード不要
-            resource.update_without_password(params)
+        resource.update_without_password(params)
         elsif params["password"].present?
-            super
+        super
         else
-            resource.update_without_password(params.except("current_password"))
+        resource.update_without_password(params.except("current_password"))
         end
+    end
+
+    protected
+
+    def oauth_request?
+        request.path.include?('/auth/') || 
+        params[:authenticity_token].blank? ||  
+        request.referrer&.include?('/auth/')  
     end
 end
