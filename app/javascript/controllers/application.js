ebc4cd563cad545f9af287c87afb28b02629fbfa
kazuta65b <hirokazu.vn.ank@gmail.com>
2025-11-15 14:45:00 +0900

    画面遷移修正

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,15 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+// app/javascript/application.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    if (menuButton && mobileMenu) {
+        menuButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+        });
+    }
+});
