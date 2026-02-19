0ae8c7be223cb68285d577e7e7cd0c912141e5d0
kazuta65b <hirokazu.vn.ank@gmail.com>
2026-02-20 01:19:00 +0900

    chart.rb 修正

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,7 +1,8 @@
 class Chart < ApplicationRecord
   belongs_to :ice_cream, optional: true
   belongs_to :user, optional: true
-  validates :sweetness, :freshness, :richness, :calorie, :ingredient_richness, :chart_type, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :sweetness, :freshness, :richness, :calorie, :ingredient_richness, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :chart_type, presence: true
 
   enum :chart_type, {
       official: "official",
