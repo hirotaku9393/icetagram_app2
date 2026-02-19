35e31ff8a99a025fbc93a8cca4efe14cf43a47a7
kazuta65b <hirokazu.vn.ank@gmail.com>
2026-02-20 01:48:19 +0900

    fix: routes.rb修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get "favorites/create"
-  get "favorites/destroy"
-  get "images/ogp"
-
   root "top#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
