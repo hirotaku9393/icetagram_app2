1753dcc0135204397080c9bcac1b73146be11c0e
kazuta65b <hirokazu.vn.ank@gmail.com>
2026-02-20 11:10:23 +0900

    fix: workflowのためのsitemap.ymlを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,31 +41,7 @@ class User < ApplicationRecord
         )
 
         user.password ||= Devise.friendly_token[0, 20]
-        user.save!
+        user.save
         user
-    end
-
-
-    # ユーザーが複数のsns連携を持っている場合("line")のようにすることで、そのサービスのプロフィールを返す
-    def social_profile(provider)
-        social_profiles.select { |sp| sp.provider == provider.to_s }.first
-    end
-
-    # userの属性を変更するメソッド
-    def set_values(omniauth)
-        return if provider.to_s != omniauth["provider"].to_s || uid != omniauth["uid"]
-        credentials = omniauth["credentials"]
-        info = omniauth["info"]
-
-        access_token = credentials["refresh_token"]
-        access_secret = credentials["secret"]
-        credentials = credentials.to_json
-        name = info["name"]
-    end
-
-    # JSONに変換して保存する
-    def set_values_by_raw_info(raw_info)
-        self.raw_info = raw_info.to_json
-        self.save!
     end
 end
