6490aa8ca43736fd9094a2e5287829bf1d1fa64c
kazuta65b <hirokazu.vn.ank@gmail.com>
2026-02-21 10:40:37 +0900

    fix: review model 修正

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   # ポリモーフィック関連付け
-  belongs_to :ice_cream, optional: true
+  belongs_to :ice_cream
   validates :content, presence: true, length: { maximum: 100 }
 end
